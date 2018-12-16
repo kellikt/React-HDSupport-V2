20e7032906f8b27b768e9bf8381d6f3991c146a6
Anthony Yarbrough <asyarb@hawaii.edu>
2018-12-16 13:08:50 -1000

    konami code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
+import konami from 'konami';
 
 import Header from './components/Header/Header';
 import SiteRouter from './components/SiteRouter';
 import { LayoutContext } from './LayoutContext';
 
-// TODO: UPDATE PATHS IN ROUTER AND LINKS
-
 class App extends Component {
+    state = {
+        easterEgg: false,
+    };
+
+    componentDidMount() {
+        new konami(() => {
+            this.setState(state => {
+                return {
+                    easterEgg: !state.easterEgg,
+                };
+            });
+        });
+    }
+
     render() {
         let value = this.context;
         const { fullScreen, loaded } = value;
@@ -15,7 +28,7 @@ class App extends Component {
         return (
             <AppContainer>
                 {loaded && (
-                    <ContentContainer fullScreen={fullScreen}>
+                    <ContentContainer fullScreen={fullScreen} easterEgg={this.state.easterEgg}>
                         <Header duration={250} />
                         <SiteRouter />
                     </ContentContainer>
@@ -28,6 +41,19 @@ class App extends Component {
 App.contextType = LayoutContext;
 
 export default App;
+
+const rotate360 = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+`;
+
+const animation = () => css`
+    animation: ${rotate360} 1s linear infinite;
+`;
 
 const AppContainer = styled.div`
     display: flex;
@@ -43,4 +69,5 @@ const ContentContainer = styled.div`
     min-height: 100vh;
     display: flex;
     flex-direction: column;
+    ${({ easterEgg }) => (easterEgg ? animation : null)};
 `;
