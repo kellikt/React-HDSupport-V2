7462ed68ea7ec807a6ebaf70b1d5057d265b6aee
kellikt <kellikt@hawaii.edu>
2023-03-21 14:31:49 -1000

    fixed view timesheets hover

--- server/index.php
+++ server/index.php
@@ -7,14 +7,14 @@ ini_set('session.gc_maxlifetime', 14400);
 session_set_cookie_params(14400);
 
 session_start();
-include "./do_auth.php";
+include "/routes/do_auth.php";
 
 if (isset($_SESSION["host"])) {
     $host = $_SESSION["host"];
 } else {
-    $host = "https://www.hawaii.edu/help/hdsupport/";
+    $host = "https://www.hawaii.edu/help/hdsupport/dev5/";
 }
 
 ?>
 
-<!doctype html><html lang="en"><head><meta charset="utf-8"/><meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"/><meta name="theme-color" content="#000000"/><link rel="manifest" href="/help/hdsupport/manifest.json"/><title>HDSupport</title><script defer="defer" src="/help/hdsupport/static/js/main.4cf0e929.js"></script><link href="/help/hdsupport/static/css/main.5d24395b.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div></body></html>
+<!doctype html><html lang="en"><head><meta charset="utf-8"/><meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no"/><meta name="theme-color" content="#000000"/><link rel="manifest" href="/help/hdsupport/dev5/manifest.json"/><title>HDSupport</title><script defer="defer" src="/help/hdsupport/dev5/static/js/main.32474753.js"></script><link href="/help/hdsupport/dev5/static/css/main.5d24395b.css" rel="stylesheet"></head><body><noscript>You need to enable JavaScript to run this app.</noscript><div id="root"></div></body></html>
